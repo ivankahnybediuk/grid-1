736721073ac6100c964753baad8f3696d04d7072
ivankahnybediuk <56602081+ivankahnybediuk@users.noreply.github.com>
2020-04-25 14:00:54 +0300

    1

---
+++ script.js
@@ -0,0 +1,23 @@
+let firstGrid=
+<div className="firstGrid">
+<div className="firstGrid__1"></div>
+<div className="firstGrid__2"></div>
+<div className="firstGrid__3"></div>
+<div className="firstGrid__4"></div>
+<div className="firstGrid__5"></div>
+<div className="firstGrid__6"></div>
+</div>
+
+let secondGrid=
+<div className="secondGrid">
+
+</div>
+
+
+// =======================Page======================================================
+let page =
+<div className="page">
+{firstGrid}
+</div>
+
+ReactDOM.render(page, document.getElementById("root"));
